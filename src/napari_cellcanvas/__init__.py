6861ff87bc7037d29e1340616f87551629666af6
Kyle Harrington <czi@kyleharrington.com>
2024-10-22 14:48:07 -0400

    Update to add experimental widget

--- src/napari_cellcanvas/__init__.py
+++ src/napari_cellcanvas/__init__.py
@@ -1,7 +1,9 @@
 from .widget import CellCanvasWidget
+from .minimal_cellcanvas import CopickPlugin as ExperimentalCellCanvasWidget
 
 __version__ = "0.0.1"
 
 __all__ = (
     "CellCanvasWidget",
+    "ExperimentalCellCanvasWidget"
 )
